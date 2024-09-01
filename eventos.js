0d45aa9ccd5dce135dafc5893910b745f0ac649a
Florencia Giménez <b3rserker1999@gmail.com>
2024-08-31 22:34:51 -0300

    agregar div, css y script

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('saludo').addEventListener('click', function() {
+        alert('¡Hola, soy el div!');
+    });
+
+});
